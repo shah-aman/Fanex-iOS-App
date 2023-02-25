42694ea89755f22f679527046398e9f765bea7cc
aman.shah@hpe.com <aman.shah@hpe.com>
2023-02-25 10:50:16 +0100

    Initial Commit

---
+++ fanex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fanex
+//
+//  Created by aman shah on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
